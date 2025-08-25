0913194e3b5533f6406346de5ee860c901814ab6
danayt <danaytesayas4@gmail.com>
2025-08-25 17:53:00 +0300

    Create profile.tsx

---
+++ chat-app/profile.tsx
@@ -0,0 +1,8 @@
+import ProfileForm from "@/components/ProfileForm";
+export default function ProfilePage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <ProfileForm />
+    </div>
+  );
+}
