f0b9a26fc5c9ebf7c553fec3255dcf2069cb8a27
Afomiya21-negash <afomiyamesfin@gmail.com>
2025-09-03 23:42:33 +0300

    another eslint

--- chat-app/eslint.config.mjs
+++ chat-app/eslint.config.mjs
@@ -18,12 +18,16 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "@next/next/no-img-element": "warn",
       "react/no-unescaped-entities": "off"
     }
   }
 ];
 
 export default eslintConfig;
+
 
 
 
